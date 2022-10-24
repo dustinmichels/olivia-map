7ee261b2b981144da5d6dbae610c5e3338f4adf5
Dustin Michels <dustin7538@gmail.com>
2022-10-24 14:47:38 -0400

    update script, cleanup notebook

--- get_natural_earth_data.sh
+++ get_natural_earth_data.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
+# create folder if missing
+echo "> Create 'data/ne' folder if missing"
 mkdir -p data/ne
+
+# remove existing files
+echo "> Remove existing zip files"
+rm -f data/ne/*.zip
 
 # admin 0 - countries
 OUT="data/ne/ne_10m_admin_0_countries.zip"
-echo "> Download 'admin 0 - countries' to: $OUT"
-curl "https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_countries.zip?version=5.1.1" \
-  -s -o $OUT
+echo "> Download 'admin 0 - countries' to:        $OUT"
+curl "https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_countries.zip" \
+  -s -Lo $OUT
 
 # admin 1 - states/provinces
-OUT="data/ne/ne_10m_admin_1_states_provinces_lines.zip"
-echo "> Download 'admin 1 - states/provinces' to $OUT"
-curl "https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_1_states_provinces_lines.zip?version=5.1.0" \
-  -s -o $OUT
+OUT="data/ne/ne_10m_admin_1_states_provinces.zip"
+echo "> Download 'admin 1 - states/provinces' to: $OUT"
+curl "https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_1_states_provinces.zip" \
+  -s -Lo $OUT
 
 # populated places (simple)
 OUT="data/ne/ne_10m_populated_places_simple.zip"
-echo "> Download 'populated places (simple)' to $OUT"
-curl "https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_populated_places_simple.zip?version=5.1.2" \
-  -s -o $OUT
+echo "> Download 'populated places (simple)' to:  $OUT"
+curl "https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_populated_places_simple.zip" \
+  -s -Lo $OUT
